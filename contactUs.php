f76cc82e7090e6284391a6e1d1765fee10f6336f
Angot Nathan <goelnathan@gmail.com>
2019-01-10 22:14:22 +0200

    sectionning the code in different include parts

--- contactUs.php
+++ contactUs.php
@@ -1,28 +1,6 @@
 <?php
-
+include './include/header.php';
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Heebo:700" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-</head>
-<body>
-    <div class="flex">
-        <div class="flex column mainWidth">
-            <header class="headerStyle flex column justifyContent">
-                <h1 class="headerContent titleStyle">YITRO</h1>
-                <h2 class="headerContent subtitleStyle">
-                אנחנו נביא את הארגון למקום אפקטיבי ויעיל יותר
-                </h2>
-            </header>
             <main>
                 <div class='unit' id='contact_us'>
                     <h3>צור איתנו קשר </h3>
@@ -48,27 +26,13 @@
                 </div>
             </main>
         </div>
-        <div class="menuStyle">
-            <div class="flex column menuStyle position">
-                <h2 class="titleInMenuStyle">YITRO</h2>
-                <ul class="menu flex column align">
-                    <li><a href="index.php">עמוד ראשי</a></li>
-                    <li><a href="">עלינו</a></li>
-                    <li><a href="">השירותים שלנו</a></li>
-                    <li><a href="contactUs.php">צור קשר</a></li>
-                </ul>
-                <div class="flex justifyContent" id="specificId">
-                    <i class="fab fa-facebook-f socialIconsStyle"></i>
-                    <i class="fab fa-twitter socialIconsStyle"></i>
-                    <i class="fab fa-pinterest-p socialIconsStyle"></i>
-                    <i class="fab fa-instagram socialIconsStyle"></i>
-                </div>
-            </div>        
-        </div>
+        <?php
+        include './include/menu.php';
+        ?>
     </div>
-    <footer>
-        <small>&copy Nathan Goel and Shlomi Hudedi - 2019</small>
-    </footer>
+    <?php
+    include './include/footer.php';
+    ?>
 </body>
 </html>
 
---
+++ include/footer.php
@@ -0,0 +1,5 @@
+<?php
+?>
+<footer>
+    <small>&copy Nathan Goel and Shlomi Hudedi - 2019</small>
+</footer>
