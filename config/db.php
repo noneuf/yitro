00a10b79589caa58f7f7cecf23e7fc5a7b425ab0
Angot Nathan <nathan@Angots-MacBook-Pro.local>
2019-01-10 20:08:43 +0200

    after database

--- config/db.php
+++ config/db.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nathan
- * Date: 2019-01-10
- * Time: 19:00
- */
+$host = '127.0.0.1';
+$db   = 'yitro';
+$user = 'root';
+$pass = 'root';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+
+
+
+/*while ($row = $stmt->fetch())
+{
+    echo $row['name'] . "<br/>";
+}*/
--- registration.php
+++ registration.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nathan
- * Date: 2019-01-10
- * Time: 19:14
- */
+include './config/db.php';
+
+$fullName = $_REQUEST['fullName'];
+$phoneNumber = $_REQUEST['phoneNumber'];
+$companyName = $_REQUEST['companyName'];
+$text = $_REQUEST['text'];
+
+//$sql = "INSERT INTO `yitro`(`name`, `phone`, `companyName`, `text`) VALUES ($fullName, $phoneNumber, $companyName, $text)";
+
+$data = [
+    'fullName' => $fullName,
+    'phoneNumber' => $phoneNumber,
+    'companyName' => $companyName,
+    'text' => $text
+];
+
+$sql = "INSERT INTO `users`(`name`, `phone`, `companyname`, `comments`) VALUES (:fullName, :phoneNumber, :companyName, :text)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute($data);
+echo "הפרטים שלך התקבלו במערכת, אנחנו ניצור איתך קשר בקרוב.";
+
